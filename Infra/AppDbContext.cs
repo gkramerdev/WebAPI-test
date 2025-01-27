615a9bc328e5068c9e083de7c9638785b6efea1d
ProgAttack <gianramiress@hotmail.com>
2025-01-27 20:40:00 -0300

    Initial project setup: created models, configured database connection, and Dockerized the application.

---
+++ Infra/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {        
+            
+        }
+
+        public DbSet<AutorModel> Autores { get; set; }
+        public DbSet<LivroModel> Livros { get; set; }
+
+     
+
+    }
+}
---
+++ Models/AutorModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI.Models
+{
+    public class AutorModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public ICollection<LivroModel> Livros { get; set; } 
+    }
+}
